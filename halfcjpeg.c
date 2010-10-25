2222e0174a51ae66d183137032f29313ec7e84e5
U-Logan-Laptop\Logan <Logan@Logan-Laptop.(none)>
2010-10-24 18:14:01 -0700

    Added reading in file information into a buffer

--- halfcjpeg.c
+++ halfcjpeg.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #define u8 unsigned char
 
@@ -42,11 +45,67 @@ int get_block(u8 ** mblock, u8 * block, int index)
     
     return 1;
 }        
+
+
+
+
     
-int main(argc, argv)
+int main(int argc, char** argv)
 {
+    // check input
+    if (argc != 5) {
+	perror("Wrong number of arguments. Exiting.");
+	exit(EXIT_FAILURE);
+    }
+    
+    // user arguments
+    char* image = argv[1];
+    char* quantFileName = argv[2];
+    int qScale = atoi(argv[3]);
+    char* outputFileName = argv[4];
+    
+    
+    // PGM header file information
+    char* head = "";
+    char* xValTemp = "";
+    int xVal = 0;
+    char* yValTemp = "";
+    int yVal = 0;
+    char* colorVal = "";
 
-	
 
+    // read input from PGM file image
+    FILE* dataFile = fopen(image,"r");
+    FILE* outputFileHandle = fopen(outputFileName,"w");
+    // read the first line of the PGM file, should be P5
+    char tempBuff[256];
+    fgets(tempBuff, 10, dataFile);
+
+    // read the second line of input, x and y pixel sizes
+    fgets(tempBuff,256,dataFile);
+
+    // get each pixel number, delimited by a space and
+    // copy x value from buffer 
+
+    //strcpy(xValTemp, temp);
+    strcpy(xValTemp,(strtok(tempBuff, " ")));
+    xVal = atoi(xValTemp);
+
+    // get and copy y value from buffer
+
+    strcpy(yValTemp, strtok(NULL," "));
+    yVal = atoi(yValTemp);
+
+    // get and copy color value from PGM file
+    fgets(tempBuff, 256, dataFile);
+    strcpy(colorVal, tempBuff);
+
+    // create buffer and store grayscale data
+    unsigned char* dataBuffer = (char* ) malloc((xVal*yVal) + 1);  
+    fgets(dataBuffer, (xVal*yVal), dataFile);
+    fclose(dataFile);
+
+
+ 
     return 1;
 }
